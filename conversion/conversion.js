c9d01096b7f46addfb25a84d9be80edf839fad92
Souvik Adak <66893928+souvikadak107@users.noreply.github.com>
2024-03-03 11:03:43 +0000

    conversion

---
+++ conversion/conversion.js
@@ -0,0 +1,34 @@
+let score="33"
+console.log(score)
+console.log(typeof(score))
+
+let numscore= Number(score)
+console.log(typeof(numscore))
+console.log(numscore)
+/* o/p
+33
+string
+number
+33
+*/
+
+
+//let we change varible like numstring... then check what happend
+
+ myscore="33abc"
+ console.log(myscore)
+ console.log(typeof(myscore))
+ 
+
+ nummyscore= Number(myscore)
+ console.log(typeof(nummyscore))
+
+ console.log(nummyscore)
+
+ /* 
+ o/p:
+33abc
+string
+number
+NaN
+*/
